6dbc287eaecf4b0f9ac2ed7011e3073adde05c1c
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-11-26 15:41:54 +0200

    konfiguraatio muutos

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
