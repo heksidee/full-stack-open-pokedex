63e2bddb03e8a31851f7cf75a2d6bab4ba9966f1
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-12-01 14:16:18 +0200

    broken version deployment

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('dist'));
 
 app.get('/health', (req, res) => {
-  res.send('still ok?');
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  ';
+  res.send('ok?');
 });
 
 app.listen(PORT, () => {
